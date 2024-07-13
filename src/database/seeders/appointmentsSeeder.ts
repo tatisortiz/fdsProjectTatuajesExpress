d62aaa5d24d8cf5d4c7e1ad35f614e03c5cddc0a
tatianaD <angietatiana167@gmail.com>
2024-07-13 19:57:10 +0200

    feat: function seeders

--- src/database/seeders/appointmentsSeeder.ts
+++ src/database/seeders/appointmentsSeeder.ts
@@ -4,7 +4,7 @@ import { Appointments } from "../models/Appointments";
 export const appointmentsSeeder = async () => {
     try {
         await AppDataSource.initialize();
-        console.log(1)
+        
         const appointment1 = new Appointments();
         appointment1.id = 1;
         appointment1.appointment_date= new Date ("2024-05-07");
@@ -12,7 +12,7 @@ export const appointmentsSeeder = async () => {
         appointment1.service_id = 2;
         await appointment1.save();
 
-       console.log(2)
+       
          const appointment2 = new Appointments();
          appointment2.id = 2;
          appointment2.appointment_date = new Date ("2024-08-07");
@@ -22,23 +22,23 @@ export const appointmentsSeeder = async () => {
 
 
 
-        // const appointment3 = new Appointments();
-        // appointment3.id = 3;
-        // appointment3.appointment_date = new Date ("2024-15-07");
-        // appointment3.user_id = 4;
-        // appointment3.service_id = 5;
-        // await appointment3.save();
+         const appointment3 = new Appointments();
+         appointment3.id = 3;
+        appointment3.appointment_date = new Date ("2024-09-12");
+        appointment3.user_id = 4;
+        appointment3.service_id = 5;
+         await appointment3.save();
 
 
-        // const appointment4 = new Appointments();
-        // appointment3.id = 4;
-        // appointment3.appointment_date = new Date ("2024-16-07");
-        // appointment3.user_id = 1;
-        // appointment3.service_id = 5;
-        // await appointment3.save();
+         const appointment4 = new Appointments();
+        appointment3.id = 4;
+         appointment3.appointment_date = new Date ("2024-05-07");
+         appointment3.user_id = 1;
+         appointment3.service_id = 5;
+         await appointment3.save();
 
         console.log("===========================");
-        console.log("Appointments seeder executed successfully");
+        console.log("Appointments seeder successfully");
         console.log("===========================");
           
     } catch (error: any) {
--- src/database/seeders/usersSeeder.ts
+++ src/database/seeders/usersSeeder.ts
@@ -5,7 +5,7 @@ import { User } from '../models/User';
 export const usersSeeder = async () => {
     try {
         await AppDataSource.initialize();
-        console.log(23455);
+        
 
         const user = new User();
         user.id = 1;
@@ -13,9 +13,9 @@ export const usersSeeder = async () => {
         user.email = "user@user.com";
         user.password = bcrypt.hashSync("123456789", 10);
         user.role_Id = 2;
-        await user.save();  // Usando directamente el método save()
+        await user.save();  
 
-        console.log(8233);
+        
 
         const adminUser = new User();
         adminUser.id = 2;
@@ -23,31 +23,31 @@ export const usersSeeder = async () => {
         adminUser.email = "admin@admin.com";
         adminUser.password = bcrypt.hashSync("123456789", 10);
         adminUser.role_Id = 2;
-        await adminUser.save();  // Usando directamente el método save()
-        console.log(3)
+        await adminUser.save();  
+        
         const superAdminn = new User();
         superAdminn.id = 3;
         superAdminn.name = "lina";
         superAdminn.email = "superadmin@superadmin.com";
         superAdminn.password = bcrypt.hashSync("123456789", 10);
         superAdminn.role_Id= 3;
-        await superAdminn.save();  // Usando directamente el método save()
-        console.log(4)
+        await superAdminn.save();  
+        
         const userOne = new User();
         userOne.id = 4;
         userOne.name = "jose";
         userOne.email = "userone@userone.com";
         userOne.password = bcrypt.hashSync("123456789", 10);
         userOne.role_Id = 2;
-        await userOne.save();  // Usando directamente el método save()
-        console.log(6)
+        await userOne.save();  
+        
         const userTatto = new User();
         userTatto.id = 5;
         userTatto.name = "jeferson";
         userTatto.email = "usertatto@usertatto.com";
         userTatto.password = bcrypt.hashSync("123456789", 10);
         userTatto.role_Id = 1;
-        await userTatto.save();  // Usando directamente el método save()
+        await userTatto.save();  
 
         console.log("==========================");
         console.log("Users seeder successfully");
